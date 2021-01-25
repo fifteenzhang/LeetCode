83f28ccbaf9242553fcd705ce206526173dadad5
fifteenzhang <799917669@qq.com>
2021-01-25 23:31:19 +0800

    初始化项目

---
+++ src/main/java/com/neil/javaleetcode/JavaleetcodeApplication.java
@@ -0,0 +1,14 @@
+package com.neil.javaleetcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class JavaleetcodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaleetcodeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/neil/javaleetcode/JavaleetcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neil.javaleetcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaleetcodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
